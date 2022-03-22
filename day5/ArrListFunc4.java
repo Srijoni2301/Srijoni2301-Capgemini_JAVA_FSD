fc23b79cdb27ba14602ef5ac380177ec78b2942b
Srijoni2301 <101315024+Srijoni2301@users.noreply.github.com>
2022-03-22 14:09:20 +0530

    Add files via upload

---
+++ day5/ArrListFunc4.java
@@ -0,0 +1,15 @@
+package day5;
+import java.util.ArrayList;
+
+public class ArrListFunc4 {
+    public static void main(String[] args){
+        ArrayList<String> animals = new ArrayList<>();
+        // Add elements
+        animals.add("Dog");
+        animals.add("Cat");
+        animals.add("Horse");
+
+        System.out.println("ArrayList: " + animals);
+    }
+
+}
